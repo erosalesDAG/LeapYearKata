2673fe41fdcba682d0a10a84ee046c744ab96829
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-03-14 23:14:14 +0000

    test: added testcases for both tests

--- LeapYearKata.Tests/LeapYearShould.cs
+++ LeapYearKata.Tests/LeapYearShould.cs
@@ -7,20 +7,25 @@ namespace LeapYearKata.Tests
         {
         }
 
-        [Test]
-        public void ReturnTrueIfDivisibleBy4()
+        [TestCase(4)]
+        [TestCase(64)]
+        [TestCase(512)]
+        public void ReturnTrueIfDivisibleBy4(int year)
         {
             LeapYear leapyear = new LeapYear(); 
-            var result = leapyear.isDivisibleBy4(4);
+            var result = leapyear.isDivisibleBy4(year);
             Assert.True(result);
         }
 
-        [Test]
-        public void returnFalseIfNotDivisibleBy4()
+        [TestCase(5)]
+        [TestCase(21)]
+        [TestCase(57)]
+        public void returnFalseIfNotDivisibleBy4(int year)
         {
             LeapYear leapyear = new LeapYear();
-            var result = leapyear.isDivisibleBy4(5);
+            var result = leapyear.isDivisibleBy4(year);
             Assert.False(result);
         }
     }
+}
 }
